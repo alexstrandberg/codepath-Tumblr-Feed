49f7f142e38c7ecad42b77f0e8f60fbcbf506339
Alex Strandberg <8642246+alexstrandberg@users.noreply.github.com>
2016-06-16 11:55:17 -0700

    Initial upload

---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  
+//
+//  Created by Alexander Strandberg on 6/16/16.
+//
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
